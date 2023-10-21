8186ffc7aa7d84a1df0fc3b6af84de9ddcf729e6
khoaminh100597 <khoaminh.energy@gmail.com>
2023-10-21 22:13:02 +0700

    update

--- datacamp_ML_app.py
+++ datacamp_ML_app.py
@@ -24,9 +24,9 @@ app_mode = st.sidebar.selectbox('Select Page', ['Home', 'Prediction'])
 
 if app_mode == 'Home':
     st.title('LOAN PREDICTION: ')
-    st.image(rf'.\examples\loan_image.jpg')
+    st.image(rf'./loan_image.jpg')
     st.markdown('Dataset: ')
-    data = pd.read_csv('.\examples\loan_dataset.csv')
+    data = pd.read_csv('./loan_dataset.csv')
     st.write(data.head())
     st.markdown('Applicant Income vs Loan Amount')
     st.bar_chart(data[['ApplicantIncome','LoanAmount']].head(20))
@@ -65,36 +65,3 @@ elif app_mode == 'Prediction':
         Semiurban = 1    
     else :        
         Rural=1
-
-data1 = {
-    'Gender': Gender,
-    'Married': Married,
-    'Dependents': [class_0, class_1, class_2, class_3],
-    'Education': Education,
-    'ApplicantIncome': ApplicantIncome,
-    'CoapplicantIncome': CoapplicantIncome,
-    'Self Employed': Self_Employed,
-    'LoanAmount': LoanAmount,
-    'Loan_Amount_Term': Loan_Amount_Term,
-    'Credit_History': Credit_History,
-    'Property_Area': [Rural, Urban, Semiurban],
-}
-
-feature_list = [ApplicantIncome,
-                CoapplicantIncome,
-                LoanAmount,
-                Loan_Amount_Term,
-                Credit_History,
-                get_value(Gender, gender_dict),
-                get_fvalue(Married),
-                data1['Dependents'][0],
-                data1['Dependents'][1],
-                data1['Dependents'][2],
-                data1['Dependents'][3],
-                get_value(Education, edu),
-                get_fvalue(Self_Employed),
-                data1['Property_Area'][0],
-                data1['Property_Area'][1],
-                data1['Property_Area'][2]]
-
-single_sample = np.array(feature_list).reshape(1,-1)
