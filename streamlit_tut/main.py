df5b5c9029081c86aeae12291f6ed12458d49144
khoaminh100597 <khoaminh.energy@gmail.com>
2023-10-26 16:51:13 +0700

    update streamlit

--- streamlit_tut/main.py
+++ streamlit_tut/main.py
@@ -3,6 +3,23 @@ import pandas as pd
 import numpy as np
 import time
 import altair as alt
+import matplotlib.pyplot as plt
 from datetime import datetime, date, time
+from vega_datasets import data
 
-st.metric(label='Temperature', value="70 °F", delta="1.2 °F")
+source = data.cars()
+
+chart = alt.Chart(source).mark_circle().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin'
+).interactive()
+
+tab1, tab2 = st.tabs(['Streamlit theme (default)',
+                      'Altair native theme'])
+
+with tab1:
+    st.altair_chart(chart, theme='streamlit', use_container_width=True)
+
+with tab2:
+    st.altair_chart(chart, theme=None, use_container_width=True)
